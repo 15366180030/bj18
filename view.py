c547ff85a6d5e02cd644d02c13ecb390a15ac516
hzy <97059286@qq.com>
2018-10-17 18:18:48 -0700

    chuangjian shitu login

--- view.py
+++ view.py
@@ -1,6 +1,9 @@
-
+from django.shortcuts import redirect
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse('index')
+
+def login(request):
+    return redirect('/index/')
